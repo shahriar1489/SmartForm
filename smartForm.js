fddaf332b60b63e31fc3e066c9622af6b51e1b57
shahriar1489 <shahriar294@gmail.com>
2017-02-13 09:29:07 +0600

    Update smartForm.js

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,14 @@
 // Your jQuery goes here
 
 var userData = {};
+
+$("#start").click(function(event){
+	$("#q1").show();
+	$("#welcome").hide();
+});
+
+$("name").on("change", function(){
+	console.log($(this).val());
+});
+
+// she used prop
