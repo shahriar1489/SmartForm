188bac8b1d0263c9edde292fe70342b7ad79c7f4
escaperoute306 <shahriar294@gmail.com>
2017-02-19 23:30:21 +0600

    incomplete

--- smartForm.js
+++ smartForm.js
@@ -1,36 +1,15 @@
-// Your jQuery goes here
-
-
-// she used prop
-
-
-//<button class="btn btn-primary btn-lg">Start Survey</button>
-/*
-$(".btn-primary").click(function(){// upon clicking the START survey button
-		// hide the contents on display
-	  // go to q1
-		("#welcome").hide();	// 13:17 NOT WORKING. hide the
-
-
-			note that before the css should be manipulated to show the contents
-
-
-		//("").show();
-});
-*/
-
 var userData = {
-	name: '',
-	email: '',
-	html: [],
-	css: [],
-	js: [],
-	htmlSkill: null,
-	cssSkill: null,
-	jsSkill: null,
-	position: "welcome"
-};
+	name: '',	// in
+	email: '',	// in
+	html: [],	// in
+	css: [],	// in
+	js: [],	// in
+	htmlSkill: null,	//
+	cssSkill: null,	//
+	jsSkill: null, //
+	position: "welcome"	// 19:35 WHAT IS THIS FOR???
 
+};
 
 $(document).ready(function(){
 
@@ -46,35 +25,55 @@ $(document).ready(function(){
 		return re.test(email);
 	}
 
-	$("#start_button").click(function(event){
 
+	//$( ".start_button" ).button( "option", "disabled", true );
+
+	$("#start_button").click(function(event){
 		$("#welcome").hide();
 		$("#q1").show();
-		$("#q1").attr("display", "block");
-
 	});	// 15:24 works
 
 	$("#q1NextButton").click(function(event){	// 17:06 not working
 
 		if(validateEmail( $("#exampleInputEmail1").val()) && validateName( $("#name").val() ))	// check if both input for name and email are valid
 		{
+			userData.name = $("#name").val();
+			userData.email = $("#exampleInputEmail1").val();
+			console.log("NAME = " + userData.name);
+			console.log("EMAIL = " + userData.email);	// 19:32 works
+
 			$("#q1").hide();	// hide the element on display
 			$("#q2").show(); // show the follwing hidden element by-
-			$("#q2").attr("display", "block");	// changing value of dispute
 		}
 
 		else{
 
 			if(validateName( $("#name").val() ) && !validateEmail( $("#exampleInputEmail1").val() )){
+				//$("#q1NextButton").prop("disabled");
+				$("#exampleInputEmail1").focus();
 				alert("ERROR: Invalid Email Input.");
+				//$("#warningMsg1").innerHTML("ERROR: Invalid Email Input.");
+				document.getElementById('warningMsg1').innerHTML="ERROR: Invalid Email Input.";
+				$('#warningMsg1').fadeOut(5000);
 			}
 
 			else if(!validateName( $("#name").val() ) && validateEmail( $("#exampleInputEmail1").val() )){
+				//$("#q1NextButton").prop("disabled");
+				$("#name").focus();
 				alert("ERROR: Invalid Name Input.");
+				//$("#warningMsg1").innerHTML("ERROR: Invalid Name Input.");
+				//document.getElementById('warningMsg1').innerHTML="ERROR: Invalid Name Input.";
+				//$('#warningMsg1').fadeOut(5000);
 			}
 
 			else if(!validateName( $("#name").val() ) && !validateEmail( $("#exampleInputEmail1").val() )){
+				//$("#q1NextButton").prop("disabled");
+				$("#name").focus();
 				alert("ERROR: Invalid Name & Email Input.");
+				//$("#warningMsg1").innerHTML("ERROR: Invalid Name & Email Input.");
+				document.getElementById('warningMsg1').innerHTML = "ERROR: Invalid Name & Email Input.";
+				$('#warningMsg1').fadeOut(5000);
+				//document.getElementById("p1").innerHTML = "New text!";
 			}
 		}
 	});	// 17:17 works
@@ -85,30 +84,343 @@ $(document).ready(function(){
 
 		if(chosen == "HTML"){
 			// q2a
-			//console.log(chosen);
-			//document.write(chosen);
 			$("#q2").hide();	// hide the element on display
 			$("#q2a").show(); // show the follwing hidden element by-
-			//$("#q2a").attr("display", "block");	// changing value of dispute
 		}
 
 		else if(chosen == "CSS") {
 			// q2b
-			//console.log(chosen);
-			//document.write(chosen);
 			$("#q2").hide();	// hide the element on display
 			$("#q2b").show(); // show the follwing hidden element by-
-			//$("#q2b").attr("display", "block");	// changing value of dispute
 		}
 
 		else{
 			// javascript q2c
-			//console.log(chosen);
-			//document.write(chosen);
 			$("#q2").hide();	// hide the element on display
 			$("#q2c").show(); // show the follwing hidden element by-
-			//$("#q2c").attr("display", "block");	// changing value of dispute
+		}
+	});	// 9:26 works
+
+	$("#htmlPrev").click(function(event){	// 00:05 this is okay
+		 var eyeD, i, temp, found = false;
+		 userData.html = [];
+		 console.log("LINE 85");
+
+		 for(i = 111; i <=114; i++)	// push the response if they are selected
+		 {
+			 eyeD = "#" + i.toString();
+			 console.log("eyeD = " + eyeD + ", " + typeof eyeD);	// 11:19 data is of string type
+
+			 // 17:32 first see if a checkbox is checked or not
+			 if( $(eyeD).prop("checked") == true){
+				  console.log(eyeD + " is checked");
+				 	userData.html.push(eyeD);
+			 }
+			 else{	// if the checkbox is unchecked
+				 if( userData.html.indexOf(eyeD) != -1){ // if the data is in the array
+					 	temp = userData.html.indexOf(eyeD);	// get the index of the element
+						userData.html.splice(temp, 1);	// remove it
+				 }
+			 }
+		 }
+
+		 for(var i = 121; i <= 124; i++)	// push the response if they are selected
+		 {
+			 eyeD = "#" + i.toString();
+			 console.log("eyeD = " + eyeD + ", " + typeof eyeD);	// 11:19 data is of string type
+
+			 // 17:32 first see if a checkbox is checked or not
+			 if( $(eyeD).prop("checked") == true){
+
+				  console.log(eyeD + " is checked");
+				 	userData.html.push(eyeD);
+			 }
+			 else{	// if the checkbox is unchecked
+				 if( userData.html.indexOf(eyeD) != -1){ // if the data is in the array
+					 	temp = userData.html.indexOf(eyeD);	// get the index of the element
+						userData.html.splice(temp, 1);	// remove it
+				 }
+			 }
+		 }
+		 console.log(userData.html);	// 11:50 there is repeat in data storage. cut that off.
+		 // 18:49 this button will work if and only if there are some responses on record
+		 if( userData.html.length >= 1){
+			 // able the button,  followed by
+			 $("#q2a").hide();	// hide the element on display
+ 			 $("#q2").show(); // show the follwing hidden element by-
+		 }
+	});
+
+	$("#htmlNext").click(function(event){	// 10:37
+		var eyeD, i, temp, found = false;
+		userData.html = [];
+		console.log("LINE 85");
+
+		for(i = 111; i <=114; i++)	// push the response if they are selected
+		{
+			console.log("LINE 88");
+
+			eyeD = "#" + i.toString();
+			console.log("eyeD = " + eyeD + ", " + typeof eyeD);	// 11:19 data is of string type
+
+			// 17:32 first see if a checkbox is checked or not
+			if( $(eyeD).prop("checked") == true){
+				 console.log(eyeD + " is checked");
+				 userData.html.push(eyeD);
+			}
+			else{	// if the checkbox is unchecked
+				if( userData.html.indexOf(eyeD) != -1){ // if the data is in the array
+					 temp = userData.html.indexOf(eyeD);	// get the index of the element
+					 userData.html.splice(temp, 1);	// remove it
+				}
+			}
 		}
 
-	});	// 9:26 works
+		for(var i = 121; i <= 124; i++)	// push the response if they are selected
+		{
+			//console.log("LINE 88");
+
+			eyeD = "#" + i.toString();
+			console.log("eyeD = " + eyeD + ", " + typeof eyeD);	// 11:19 data is of string type
+
+			// 17:32 first see if a checkbox is checked or not
+			if( $(eyeD).prop("checked") == true){
+				 console.log(eyeD + " is checked");
+				 userData.html.push(eyeD);
+			}
+			else{	// if the checkbox is unchecked
+				if( userData.html.indexOf(eyeD) != -1){ // if the data is in the array
+					 temp = userData.html.indexOf(eyeD);	// get the index of the element
+					 userData.html.splice(temp, 1);	// remove it
+				}
+			}
+		}
+		console.log(userData.html);	// 11:50 there is repeat in data storage. cut that off.
+		if( userData.html.length >= 1){
+			// able the button,  followed by
+			$("#q2a").hide();	// hide the element on display
+			$("#q3").show(); // show the follwing hidden element by-
+		}
+	});
+
+	$("#cssPrev").click(function(event){	// 10:49
+		var eyeD, i, temp, found = false;
+		userData.css = [];
+		//console.log("LINE 85");
+
+		for(i = 211; i <=214; i++)	// push the response if they are selected
+		{
+			eyeD = "#" + i.toString();
+			console.log("eyeD = " + eyeD + ", " + typeof eyeD);	// 11:19 data is of string type
+
+			// 17:32 first see if a checkbox is checked or not
+			if( $(eyeD).prop("checked") == true){
+				 console.log(eyeD + " is checked");
+				 userData.css.push(eyeD);
+			}
+			else{	// if the checkbox is unchecked
+				if( userData.css.indexOf(eyeD) != -1){ // if the data is in the array
+					 temp = userData.css.indexOf(eyeD);	// get the index of the element
+					 userData.css.splice(temp, 1);	// remove it
+				}
+			}
+		}
+
+		for(var i = 221; i <= 224; i++)	// push the response if they are selected
+		{
+			eyeD = "#" + i.toString();
+			console.log("eyeD = " + eyeD + ", " + typeof eyeD);	// 11:19 data is of string type
+
+			// 17:32 first see if a checkbox is checked or not
+			if( $(eyeD).prop("checked") == true){
+
+				 console.log(eyeD + " is checked");
+				 userData.css.push(eyeD);
+			}
+			else{	// if the checkbox is unchecked
+				if( userData.css.indexOf(eyeD) != -1){ // if the data is in the array
+					 temp = userData.css.indexOf(eyeD);	// get the index of the element
+					 userData.css.splice(temp, 1);	// remove it
+				}
+			}
+		}
+		console.log(userData.css);	// 11:50 there is repeat in data storage. cut that off.
+		if( userData.css.length >= 1){
+			// able the button,  followed by
+			$("#q2b").hide();	// hide the element on display
+			$("#q2").show(); // show the follwing hidden element by-
+		}
+	});
+
+	$("#cssNext").click(function(event){	// 10:37
+		var eyeD, i, temp, found = false;
+		userData.css = [];
+		console.log("LINE 85");
+
+		for(i = 211; i <=214; i++)	// push the response if they are selected
+		{
+			console.log("LINE 183");
+
+			eyeD = "#" + i.toString();
+			console.log("eyeD = " + eyeD + ", " + typeof eyeD);	// 11:19 data is of string type
+
+			// 17:32 first see if a checkbox is checked or not
+			if( $(eyeD).prop("checked") == true){
+				 console.log(eyeD + " is checked");
+				 userData.css.push(eyeD);
+			}
+			else{	// if the checkbox is unchecked
+				if( userData.css.indexOf(eyeD) != -1){ // if the data is in the array
+					 temp = userData.css.indexOf(eyeD);	// get the index of the element
+					 userData.css.splice(temp, 1);	// remove it
+				}
+			}
+		}
+
+		for(var i = 221; i <= 224; i++)	// push the response if they are selected
+		{
+			console.log("LINE 88");
+
+			eyeD = "#" + i.toString();
+			console.log("eyeD = " + eyeD + ", " + typeof eyeD);	// 11:19 data is of string type
+
+			// 17:32 first see if a checkbox is checked or not
+			if( $(eyeD).prop("checked") == true){
+
+				 console.log(eyeD + " is checked");
+				 userData.css.push(eyeD);
+			}
+			else{	// if the checkbox is unchecked
+				if( userData.css.indexOf(eyeD) != -1){ // if the data is in the array
+					 temp = userData.css.indexOf(eyeD);	// get the index of the element
+					 userData.css.splice(temp, 1);	// remove it
+				}
+			}
+		}
+		console.log(userData.css);	// 11:50 there is repeat in data storage. cut that off.
+		if( userData.css.length >= 1){
+			// able the button,  followed by
+			$("#q2b").hide();	// hide the element on display
+			$("#q3").show(); // show the follwing hidden element by-
+		}
+	});
+
+	$("#jvsPrev").click(function(event){	// 10:37
+
+		var eyeD, i, temp, found = false;
+		userData.js = [];
+
+		for(i = 311; i <= 314; i++)	// push the response if they are selected
+		{
+			eyeD = "#" + i.toString();
+			console.log("eyeD = " + eyeD + ", " + typeof eyeD);	// 11:19 data is of string type
+
+			if( $(eyeD).prop("checked") == true){	// 17:32 first see if a checkbox is checked or not
+				 console.log(eyeD + " is checked");
+				 userData.js.push(eyeD);
+			}
+			else{	// if the checkbox is unchecked
+				if( userData.js.indexOf(eyeD) != -1){ // if the data is in the array
+					 temp = userData.js.indexOf(eyeD);	// get the index of the element
+					 userData.js.splice(temp, 1);	// remove it
+				}
+			}
+		}
+
+		for(i = 321; i <= 324; i++)	// push the response if they are selected
+		{
+			console.log("LINE 183");
+
+			eyeD = "#" + i.toString();
+			console.log("eyeD = " + eyeD + ", " + typeof eyeD);	// 11:19 data is of string type
+
+			// 17:32 first see if a checkbox is checked or not
+			if( $(eyeD).prop("checked") == true){
+				 console.log(eyeD + " is checked");
+				 userData.js.push(eyeD);
+			}
+			else{	// if the checkbox is unchecked
+				if( userData.js.indexOf(eyeD) != -1){ // if the data is in the array
+					 temp = userData.js.indexOf(eyeD);	// get the index of the element
+					 userData.js.splice(temp, 1);	// remove it
+				}
+			}
+		}
+		console.log(userData.js);	// 11:50 there is repeat in data storage. cut that off.
+		if( userData.html.length >= 1){
+			// able the button,  followed by
+			$("#q2c").hide();	// hide the element on display
+			$("#q2").show(); // show the follwing hidden element by-
+		}
+	});	// 19:06 works
+
+	$("#jvsNext").click(function(event){	// 10:37
+
+		var eyeD, i, temp, found = false;
+		userData.js = [];
+
+		for(i = 311; i <= 314; i++)	// push the response if they are selected
+		{
+			eyeD = "#" + i.toString();
+			console.log("eyeD = " + eyeD + ", " + typeof eyeD);	// 11:19 data is of string type
+
+			if( $(eyeD).prop("checked") == true){	// 17:32 first see if a checkbox is checked or not
+				 console.log(eyeD + " is checked");
+				 userData.js.push(eyeD);
+			}
+			else{	// if the checkbox is unchecked
+				if( userData.js.indexOf(eyeD) != -1){ // if the data is in the array
+					 temp = userData.js.indexOf(eyeD);	// get the index of the element
+					 userData.js.splice(temp, 1);	// remove it
+				}
+			}
+		}
+
+		for(i = 321; i <= 324; i++)	// push the response if they are selected
+		{
+			console.log("LINE 183");
+
+			eyeD = "#" + i.toString();
+			console.log("eyeD = " + eyeD + ", " + typeof eyeD);	// 11:19 data is of string type
+
+			// 17:32 first see if a checkbox is checked or not
+			if( $(eyeD).prop("checked") == true){
+				 console.log(eyeD + " is checked");
+				 userData.js.push(eyeD);
+			}
+			else{	// if the checkbox is unchecked
+				if( userData.js.indexOf(eyeD) != -1){ // if the data is in the array
+					 temp = userData.js.indexOf(eyeD);	// get the index of the element
+					 userData.js.splice(temp, 1);	// remove it
+				}
+			}
+		}
+		console.log(userData.js);	// 11:50 there is repeat in data storage. cut that off.
+		if( userData.js.length >= 1){
+			$("#q2c").hide();
+			$("#q3").show();
+		}
+	});
+
+	$("#q3Prev").click(function(event){
+		 $("#q3").hide();
+		 $("#q2").show();
+	});
+
+	$("#q3Next").click(function(event){
+		$("#q3").hide();
+		$("#thanks").show();
+	});
+
+	$("#viewAns").click(function(event){
+		//$("$viewAns").hide();
+		//$("").show();
+	});
+
+	$("#delAns").click(function(event){
+		$("#thanks").hide();
+		$("#welcome").show();
+	});
+
+
 });
