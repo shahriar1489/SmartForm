7281493cf4f994310f8518674c283425b8f30be9
escaperoute306 <shahriar294@gmail.com>
2017-02-23 21:12:17 +0600

    A long way to go

--- smartForm.js
+++ smartForm.js
@@ -8,27 +8,21 @@ var userData = {
 	cssSkill: null,	//
 	jsSkill: null, //
 	position: "welcome"	// 19:35 WHAT IS THIS FOR???
-
 };
 
 $(document).ready(function(){
 
-	function validateName(name)
-	{
+	function validateName(name){
 		var re = /^[A-z ]+$/;	// 15:04
 		return re.test(name);
 	}
 
-	function validateEmail(email)
-	{
+	function validateEmail(email){
 		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		return re.test(email);
 	}
 
-
-	//$( ".start_button" ).button( "option", "disabled", true );
-
-	$("#start_button").click(function(event){
+$("#start_button").click(function(event){
 		$("#welcome").hide();
 		$("#q1").show();
 	});	// 15:24 works
@@ -53,8 +47,8 @@ $(document).ready(function(){
 				$("#exampleInputEmail1").focus();
 				alert("ERROR: Invalid Email Input.");
 				//$("#warningMsg1").innerHTML("ERROR: Invalid Email Input.");
-				document.getElementById('warningMsg1').innerHTML="ERROR: Invalid Email Input.";
-				$('#warningMsg1').fadeOut(5000);
+				//document.getElementById('warningMsg1').innerHTML="ERROR: Invalid Email Input.";
+				//$('#warningMsg1').fadeOut(5000);
 			}
 
 			else if(!validateName( $("#name").val() ) && validateEmail( $("#exampleInputEmail1").val() )){
@@ -62,8 +56,9 @@ $(document).ready(function(){
 				$("#name").focus();
 				alert("ERROR: Invalid Name Input.");
 				//$("#warningMsg1").innerHTML("ERROR: Invalid Name Input.");
-				//document.getElementById('warningMsg1').innerHTML="ERROR: Invalid Name Input.";
-				//$('#warningMsg1').fadeOut(5000);
+				document.getElementById('warningMsg1').innerHTML="ERROR: Invalid Name Input.";
+				$('#warningMsg1').fadeOut(5000);
+				document.getElementById('warningMsg1').innerHTML=" ";	// 15:33 empty the content
 			}
 
 			else if(!validateName( $("#name").val() ) && !validateEmail( $("#exampleInputEmail1").val() )){
@@ -73,29 +68,23 @@ $(document).ready(function(){
 				//$("#warningMsg1").innerHTML("ERROR: Invalid Name & Email Input.");
 				document.getElementById('warningMsg1').innerHTML = "ERROR: Invalid Name & Email Input.";
 				$('#warningMsg1').fadeOut(5000);
-				//document.getElementById("p1").innerHTML = "New text!";
+				document.getElementById('warningMsg1').innerHTML = " ";
 			}
 		}
 	});	// 17:17 works
 
 	$(".fet").click(function(event){	// any of the FrontEndTechnology clicked
-
 		var chosen = $(this).text();	// know the one chosen
 
 		if(chosen == "HTML"){
-			// q2a
 			$("#q2").hide();	// hide the element on display
 			$("#q2a").show(); // show the follwing hidden element by-
 		}
-
 		else if(chosen == "CSS") {
-			// q2b
 			$("#q2").hide();	// hide the element on display
 			$("#q2b").show(); // show the follwing hidden element by-
 		}
-
 		else{
-			// javascript q2c
 			$("#q2").hide();	// hide the element on display
 			$("#q2c").show(); // show the follwing hidden element by-
 		}
@@ -104,7 +93,7 @@ $(document).ready(function(){
 	$("#htmlPrev").click(function(event){	// 00:05 this is okay
 		 var eyeD, i, temp, found = false;
 		 userData.html = [];
-		 console.log("LINE 85");
+		// console.log("LINE 85");
 
 		 for(i = 111; i <=114; i++)	// push the response if they are selected
 		 {
@@ -154,11 +143,11 @@ $(document).ready(function(){
 	$("#htmlNext").click(function(event){	// 10:37
 		var eyeD, i, temp, found = false;
 		userData.html = [];
-		console.log("LINE 85");
+	//	console.log("LINE 85");
 
 		for(i = 111; i <=114; i++)	// push the response if they are selected
 		{
-			console.log("LINE 88");
+		//	console.log("LINE 88");
 
 			eyeD = "#" + i.toString();
 			console.log("eyeD = " + eyeD + ", " + typeof eyeD);	// 11:19 data is of string type
@@ -196,10 +185,13 @@ $(document).ready(function(){
 			}
 		}
 		console.log(userData.html);	// 11:50 there is repeat in data storage. cut that off.
-		if( userData.html.length >= 1){
-			// able the button,  followed by
+		if( userData.html.length >= 1 && userData.css.length >= 1 && userData.js.length >= 1)	// 16:12 think about this conditional statement
+		{
 			$("#q2a").hide();	// hide the element on display
 			$("#q3").show(); // show the follwing hidden element by-
+		}else{
+			$('#q2a').hide();
+			$('#q2').show();
 		}
 	});
 
@@ -255,11 +247,11 @@ $(document).ready(function(){
 	$("#cssNext").click(function(event){	// 10:37
 		var eyeD, i, temp, found = false;
 		userData.css = [];
-		console.log("LINE 85");
+	//	console.log("LINE 85");
 
 		for(i = 211; i <=214; i++)	// push the response if they are selected
 		{
-			console.log("LINE 183");
+			//console.log("LINE 183");
 
 			eyeD = "#" + i.toString();
 			console.log("eyeD = " + eyeD + ", " + typeof eyeD);	// 11:19 data is of string type
@@ -279,7 +271,7 @@ $(document).ready(function(){
 
 		for(var i = 221; i <= 224; i++)	// push the response if they are selected
 		{
-			console.log("LINE 88");
+			//console.log("LINE 88");
 
 			eyeD = "#" + i.toString();
 			console.log("eyeD = " + eyeD + ", " + typeof eyeD);	// 11:19 data is of string type
@@ -298,11 +290,15 @@ $(document).ready(function(){
 			}
 		}
 		console.log(userData.css);	// 11:50 there is repeat in data storage. cut that off.
-		if( userData.css.length >= 1){
-			// able the button,  followed by
+		if( userData.html.length >= 1 && userData.css.length >= 1 && userData.js.length >= 1)	// 16:12 think about this conditional statement
+		{
 			$("#q2b").hide();	// hide the element on display
 			$("#q3").show(); // show the follwing hidden element by-
+		}else{
+			$('#q2b').hide();
+			$('#q2').show();
 		}
+
 	});
 
 	$("#jvsPrev").click(function(event){	// 10:37
@@ -329,7 +325,7 @@ $(document).ready(function(){
 
 		for(i = 321; i <= 324; i++)	// push the response if they are selected
 		{
-			console.log("LINE 183");
+			//console.log("LINE 183");
 
 			eyeD = "#" + i.toString();
 			console.log("eyeD = " + eyeD + ", " + typeof eyeD);	// 11:19 data is of string type
@@ -378,7 +374,7 @@ $(document).ready(function(){
 
 		for(i = 321; i <= 324; i++)	// push the response if they are selected
 		{
-			console.log("LINE 183");
+			//console.log("LINE 183");
 
 			eyeD = "#" + i.toString();
 			console.log("eyeD = " + eyeD + ", " + typeof eyeD);	// 11:19 data is of string type
@@ -396,9 +392,13 @@ $(document).ready(function(){
 			}
 		}
 		console.log(userData.js);	// 11:50 there is repeat in data storage. cut that off.
-		if( userData.js.length >= 1){
-			$("#q2c").hide();
-			$("#q3").show();
+		if( userData.html.length >= 1 && userData.css.length >= 1 && userData.js.length >= 1)	// 16:12 think about this conditional statement
+		{
+			$("#q2c").hide();	// hide the element on display
+			$("#q3").show(); // show the follwing hidden element by-
+		}else{
+			$('#q2c').hide();
+			$('#q2').show();
 		}
 	});
 
@@ -407,20 +407,80 @@ $(document).ready(function(){
 		 $("#q2").show();
 	});
 
+	$("#q3Prev").click(function(event){
+
+		var var1 = var2 = var3 = null;
+
+		var1 = $("input[name='radio1']:checked").val(); // $("input[name='gender']:checked").val();
+		userData.htmlSkill = var1;
+		console.log("html skill: " + userData.htmlSkill);
+
+		var2 = $("input[name='radio2']:checked").val();
+		userData.cssSkill = var2;
+		console.log("css skill: " + userData.cssSkill);
+
+		var3 =  $("input[name='radio3']:checked").val();
+		userData.jsSkill = var3;
+		console.log("jvs skill: " + userData.jsSkill + " " + var3);
+
+		if(userData.htmlSkill!=null && userData.cssSkill!=null && userData.jsSkill!=null){
+			$("#q3").hide();
+			$("#q2").show();
+		}
+		else{
+			alert("Make sure you have chosen your expertise for HTML, CSS, and JavaScript.");
+		}
+	});
+
 	$("#q3Next").click(function(event){
-		$("#q3").hide();
-		$("#thanks").show();
+		var var1 = var2 = var3 = null;
+
+		var1 = $("input[name='radio1']:checked").val();
+		userData.htmlSkill = var1;
+		console.log("html skill: " + userData.htmlSkill);
+
+		var2 = $("input[name='radio2']:checked").val();
+		userData.cssSkill = var2;
+		console.log("css skill: " + userData.cssSkill);
+
+		var3 =  $("input[name='radio3']:checked").val();
+		userData.jsSkill = var3;
+		console.log("jvs skill: " + userData.jsSkill);
+
+		if(userData.htmlSkill!=null && userData.cssSkill!=null && userData.jsSkill!=null){
+			$("#q3").hide();
+			$("#thanks").show();
+		}
+		else{
+			alert("Make sure you have chosen your expertise for HTML, CSS, and JavaScript.");
+		}
 	});
 
 	$("#viewAns").click(function(event){
-		//$("$viewAns").hide();
-		//$("").show();
+		$("#thanks").hide();
+		$("#showRes").show();
 	});
 
 	$("#delAns").click(function(event){
+		userData.name= userData.email = userData.htmlSkill = userData.jsSkill = userData.cssSkill= '';
+		userData.html = userData.css = userData.js = [];
+		position = "welcome";
+
+		console.log(userData);
+
 		$("#thanks").hide();
 		$("#welcome").show();
+
+		// clear the text in email and email
+		$("#name").val('');
+		$("#exampleInputEmail1").val('');
+
+		// uncheck the checkbox
+		$("input[name='likeHTML']").removeAttr("checked");	// 21:05
+		$("input[name='likeCSS']").removeAttr("checked");	// 21:05 
+		$("input[name='likeHTML']").removeAttr("checked");	// 21:05
+		// make sure previous answers are unchecked
+		//$("input[name='radio2']:checked").val();	// uncheck the radio buttons
+
 	});
-
-
 });
